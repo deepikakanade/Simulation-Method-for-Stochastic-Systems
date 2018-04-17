6f5858598e022fa516b1a1425af2fb7fb394c3e0
deepikakanade <35501056+deepikakanade@users.noreply.github.com>
2018-04-17 03:33:27 -0700

    Update Q1a.m

--- Homework1/Q1a.m
+++ Homework1/Q1a.m
@@ -1,55 +1,44 @@
 clc;
+close all;
 
 %Initialization of variables
-size = 500;
-prob = 0.5;
-samp = 100;
-sample = zeros(samp,1);
-count = zeros(size,1);
-count_final = zeros(samp,1);
-max = 0;
-temp= 0;
+size = 10;
+x = rand(size);   %To generate 100 uniform random numbers
+A=2;
+count = zeros(size*size,1);
+count_final = zeros(A,1);
+count1=0;
+count0=0;
 
-%Generate Random samples and find longest run of heads in all the trials
-for i = 1:size
-    max = 0;
-    temp = 0;
-    for j = 1:samp
-      A(j) = rand(1);
-        if A(j)>prob
-            A(j) = 1;
-        else
-            A(j) = 0;
-        end
-    end
-    for j = 1:samp-1
-        if A(j) == 1 && A(j+1) == 1
-            temp = temp+ 1;
-        else
-            temp = 0;
-        end
-        if max < temp
-            max = temp;
-        end
-    end
-    count(i) = max;
-end
-
-%Count the number of longest run of Heads for all samples
-for i = 1:samp+1
-    for j= 1:size
-        if count(j) == i-1
-            count_final(i) = count_final(i) + 1;
-        end
+%Generate a fair Bernoulli trial for 100 samples
+for i=1:100
+    if(x(i)>=0.5)
+        count(i)=1;
+        count1=count1+1;
+    else
+        count(i)=0;
+        count0=count0+1;
     end
 end
 
-%Plotting Histogram
-figure(1);
-bar(1:samp,count_final');
-xlim([1,20]);
-xlabel('Longest run');
-ylabel('Count');
-title('Longest Run of Heads in 100 Bernoulli samples');
+%count successes for all samples
+for j = 1:size*size
+    for i = 1:A
+       if count(j) == i-1
+           count_final(i,1) = count_final(i,1)+1;
+       end
+    end
+end
+
+disp("No. of Successes are: ");
+disp(count1)
+disp("No. of Failures are: ");
+disp(count0);
+
+%Plotting Histogram of the data
+bar(0:1,count_final);
+xlabel("Success=1 and Failure=0");
+ylabel("Count for each Sucess/Failure");
+title("Routine for one fair Bernoulli Trial for 100 samples");
 
 
